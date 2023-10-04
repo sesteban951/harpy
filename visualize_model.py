6a26e28fb6a6b8c9da1eacb0603c79ce567c1b0b
Vince Kurtz <vjkurtz@gmail.com>
2023-10-04 10:17:32 -0700

    add a little script to do the visualization

---
+++ visualize_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+##
+#
+# Use Drake's ModelVisualizer to show the model, collision geometries, inertias,
+# etc. in MeshCat.
+#
+##
+
+from pydrake.all import ModelVisualizer, StartMeshcat
+
+urdf_path = "./models/urdf/harpy.urdf"
+
+meshcat = StartMeshcat()
+visualizer = ModelVisualizer(meshcat=meshcat)
+visualizer.parser().AddModels(urdf_path)
+
+visualizer.Run()
+
