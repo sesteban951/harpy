786496f1f234b54de0266ac71fbbba02869e938b
Vince Kurtz <vjkurtz@gmail.com>
2023-10-04 20:14:59 -0700

    add actuation and an implicit PD controller

--- simulate.py
+++ simulate.py
@@ -28,10 +28,17 @@ builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlant(config, builder)
 
 # Create the harpy model
-Parser(plant).AddModels(model_file)
+harpy = Parser(plant).AddModels(model_file)[0]
 body_A = plant.GetBodyByName("FootLeftBall")
 body_B = plant.GetBodyByName("TibiaLeftBall")
-# TODO: add distance constraint
+plant.AddDistanceConstraint(
+    plant.GetBodyByName("FootLeftBall"), [0, 0, 0],
+    plant.GetBodyByName("TibiaLeftBall"), [0, 0, 0],
+    0.32)
+plant.AddDistanceConstraint(
+    plant.GetBodyByName("FootRightBall"), [0, 0, 0],
+    plant.GetBodyByName("TibiaRightBall"), [0, 0, 0],
+    0.32)
 
 # Add a flat ground with friction
 ground_props = ProximityProperties()
@@ -52,9 +59,36 @@ plant.RegisterCollisionGeometry(
 
 # Turn off gravity
 #plant.mutable_gravity_field().set_gravity_vector([0, 0, 0])
+
+# Implicit PD
+Kp = 50 * np.ones(plant.num_actuators())
+Kd = 5 * np.ones(plant.num_actuators())
+
+actuator_indices = []
+for i in range(plant.num_actuators()):
+    actuator_index = JointActuatorIndex(i)
+    actuator_indices.append(actuator_index)
+
+for actuator_index, Kp, Kd in zip(actuator_indices, Kp, Kd):
+    plant.get_joint_actuator(actuator_index).set_controller_gains(
+        PdControllerGains(p=Kp, d=Kd)
+    )
+
 plant.Finalize()
 
 # TODO: add simple PD controller
+controller = builder.AddSystem(
+        ConstantVectorSource(np.zeros(plant.num_actuators())))
+builder.Connect(
+        controller.get_output_port(),
+        plant.get_actuation_input_port())
+
+# Desired state sender
+desired_state_sender = builder.AddSystem(
+        ConstantVectorSource(np.zeros(16)))
+builder.Connect(
+        desired_state_sender.get_output_port(),
+        plant.get_desired_state_input_port(harpy))
 
 AddDefaultVisualization(builder, meshcat)
 diagram = builder.Build()
@@ -78,3 +112,4 @@ meshcat.StartRecording()
 simulator.AdvanceTo(sim_time)
 meshcat.PublishRecording()
 
+print("done")
