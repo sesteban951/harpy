setup 3D IK. fixed rotation axis on left thruster

controller.py
@@ -68,6 +68,44 @@ class Controller(LeafSystem):
                 lambda context, output: output.set_value(
                     self._cache.Eval(context)["thrust"]),
                 prerequisites_of_calc={self._cache.ticket()})
+        
+        # Store some useful frames
+        self.torso_frame = self.plant.GetFrameByName("Torso")
+        self.left_foot_frame = self.plant.GetFrameByName("FootLeft")
+        self.right_foot_frame = self.plant.GetFrameByName("FootRight")
+
+        # instantiate IK solver object
+        self.ik = InverseKinematics(self.plant)
+        
+        # Add distance constraints to IK for the 4-bar linkages
+        self.ik.AddPointToPointDistanceConstraint(
+                self.plant.GetFrameByName("BallTarsusLeft"), [0, 0, 0],
+                self.plant.GetFrameByName("BallFemurLeft"), [0, 0, 0], 0.32, 0.32)
+        self.ik.AddPointToPointDistanceConstraint(
+                self.plant.GetFrameByName("BallTarsusRight"), [0, 0, 0],
+                self.plant.GetFrameByName("BallFemurRight"), [0, 0, 0], 0.32, 0.32)
+
+        # inverse kinematics solver settings
+        self.epsilon_feet = 0.001   # foot position tolerance     [m]
+        self.epsilon_base = 0.01    # torso position tolerance    [m]
+        self.epsilon_orient = 0.1   # torso orientation tolerance [rad]
+        self.tol_feet = np.array([[self.epsilon_feet], [np.inf], [self.epsilon_feet]])
+        self.tol_base = np.array([[np.inf], [np.inf], [self.epsilon_base]])
+
+    def DoInverseKinematics(self, p_right,p_left,p_torso,rpy_torso):
+        """
+        Solve an inverse kinematics problem, reporting joint angles that will
+        correspond to the desired positions of the feet and torso in the world.
+
+        Args:
+            p_left: desired position of the left foot in the world frame
+            p_right: desired position of the right foot in the world frame
+            p_base: desired position of the torso in the world frame
+            r_torso: desired orientation of the torso in the world frame
+        Returns:
+            q: Joint angles that set the feet and torso where we want them
+        """
+        pass
 
     def CalcOutput(self, context):
         """
@@ -82,15 +120,20 @@ class Controller(LeafSystem):
         self.plant.SetPositionsAndVelocities(self.plant_context, x_hat)
 
         # Feed-forward joint torques
-        tau_ff = np.zeros(8)
+        tau_ff = np.array([0, 0,   # Thruster: right, left
+                           0, 0,   # Hip: right roll, left roll
+                           0, 0,   # Hip: right pitch, left pitch 
+                           0, 0])  # Knee: right pitch, left pitch 
 
         # Target joint angles and velocities
-        q_nom = np.array([
-            0, 0,   # thrusters
-            0, 0,   # hip ad/ab
-            0, 0,   # hip front/back
-            0, 0])  # knee
-        v_nom = np.zeros(8)
+        q_nom = np.array([0, 0,   # Thruster: right, left [rad]
+                          0, 0,   # Hip: right roll, left roll [rad]
+                          0, 0,   # Hip: right pitch, left pitch [rad]
+                          0, 0])  # Knee: right pitch, left pitch [rad]
+        v_nom = np.array([0, 0,   # Thruster: right, left
+                          0, 0,   # Hip: right roll, left roll
+                          0, 0,   # Hip: right pitch, left pitch 
+                          0, 0])  # Knee: right pitch, left pitch 
         x_nom = np.block([q_nom, v_nom])
 
         # Forces applied by the thrusters